c89501749705b2bb56395236a651c5df89e4fab0
Agustin <agusnespral@gmail.com>
2024-02-03 23:00:07 -0300

    write pseudo code for test case structure and steps

---
+++ cypress/e2e/aliExpress/checkStock.cy.js
@@ -0,0 +1,13 @@
+/// <reference types = "cypress" />
+
+describe('Modak Challenge - Ali Express', () => {
+    it('Validate second item from second results page when searching for "instax mini" has at least 1 item to be bought', () => {
+    
+        // 1. visit page
+        // 2. search bar locator + type instax mini
+        // 3. second page locator + click
+        // 4. second item locator + click
+        // 5. stock amount locator + assertion to validate that is > 0 
+        
+    });
+});
