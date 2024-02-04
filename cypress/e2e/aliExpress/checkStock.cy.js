50645731c19d57510a34829e04c03c07912188b0
Agustin <agusnespral@gmail.com>
2024-02-04 18:35:04 -0300

    edit comments

--- cypress/e2e/aliExpress/checkStock.cy.js
+++ cypress/e2e/aliExpress/checkStock.cy.js
@@ -4,15 +4,14 @@ import { checkStock } from "../../support/pages/aliExpressPage";
 describe('Modak Challenge - Ali Express', () => {
     it('Validate second item from second results page when searching for "instax mini" has at least 1 item to be bought', () => {
     
-        // 1. visit page
         cy.visit("/");
-        // 2. search bar locator + type instax mini
+        
         checkStock.inputSearchText("instax mini{enter}");
-        // 3. second page locator + click
+        
         checkStock.clickOnPagination("2");
-        // 4. second item locator + click
+        
         checkStock.clickOnItemCard(1)
-        // 5. stock amount locator + assertion to validate that is > 0 
+        
         checkStock.elements.stockAmount().then(value => {
             expect(parseInt(value)).to.be.greaterThan(0);
         })
